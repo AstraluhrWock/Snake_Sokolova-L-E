1669aad2257ca741cea3a51601c0c949f9b46c9d
Lubov Sokolova <falke.liebe@gmail.com>
2023-06-06 10:48:51 +0300

    add snake mvvm

---
+++ Assets/Scripts/Model/FoodModel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+internal class FoodModel
+{
+    private Vector3 _position;
+
+    public Vector3 Position => _position;
+
+    public void GenerateNewPosition(int maxX, int maxY)
+    {
+        _position = new Vector3(Random.Range(0, maxX), Random.Range(0, maxY), 0);
+    }
+
+}
---
+++ Assets/Scripts/ViewModel/FoodViewModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+internal class FoodViewModel
+{
+    private FoodModel _model;
+
+    public FoodViewModel(FoodModel foodModel)
+    {
+        _model = foodModel;
+    }
+    public Vector3 Position => _model.Position;
+}
